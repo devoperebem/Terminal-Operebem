3b463fd6163752565db764d80b4cd1f352826537
devoperebem <patrickgsiotti@gmail.com>
2025-12-08 16:56:45 -0300

    IA - FIX: Removendo Bootstrap duplicado que impedia menu mobile de fechar

--- src/Views/home/index.php
+++ src/Views/home/index.php
@@ -1007,8 +1007,6 @@ html.all-black .hero-stat-icon {
 <?php
 $content = ob_get_clean();
 $scripts = <<<'SCRIPTS'
-<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 <script src="/sdk/captcha-sdk.js"></script>
 <script src="/assets/js/register-6steps.js"></script>
 <script src="/assets/js/index-interactive.js"></script>
